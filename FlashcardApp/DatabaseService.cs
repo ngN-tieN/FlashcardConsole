da12dd90e275b9a7c02b1769b91fd97b2d8af4b1
tien <nq2017.nguyentien081202@gmail.com>
2024-03-27 16:42:37 +0700

    wrote ConnectToDB()

--- FlashcardApp/DatabaseService.cs
+++ FlashcardApp/DatabaseService.cs
@@ -1,10 +1,10 @@
 
 using Microsoft.Data.SqlClient;
 
-class MSSQLDatabase
+internal class MSSQLDatabase
 {
     string connectionString;
-    MSSQLDatabase()
+    public MSSQLDatabase()
     {
         this.connectionString = ConfigurationService.GetSqlConnectionString("mssql");
     }
@@ -14,6 +14,43 @@ class MSSQLDatabase
             connection.Open();
             var tableCmd = connection.CreateCommand();
             //need to be worked on
+            string command = @"IF OBJECT_ID('STACKS') IS NULL
+                            BEGIN
+	                        CREATE TABLE STACKS
+	                        (
+		                        id INT PRIMARY KEY NOT NULL IDENTITY(1,1),
+		                        name varchar(255) unique
+	                        )
+                            END";
+            tableCmd.CommandText = command;
+            try
+            {
+                tableCmd.ExecuteNonQuery();
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("Database error. Please try again");
+            }
+            command = @"IF (OBJECT_ID('CARD') IS NULL AND OBJECT_ID('STACKS') IS NOT NULL)	
+                        BEGIN
+	                    CREATE TABLE CARDS
+	                    (
+		                    id INT PRIMARY KEY NOT NULL IDENTITY(1,1),
+		                    front TEXT,
+		                    back TEXT, 
+		                    stack_id INT FOREIGN KEY REFERENCES STACKS(id)
+	                    )
+                        END";
+            tableCmd.CommandText = command;
+             try
+            {
+                tableCmd.ExecuteNonQuery();
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("Database error. Please try again");
+            }
+            connection.Close();
         }
     }
 }
