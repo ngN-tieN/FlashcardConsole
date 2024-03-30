b12b26f5cf71f0ee97a03c8fdc3b08f0ed551d99
tien <nq2017.nguyentien081202@gmail.com>
2024-03-30 23:03:30 +0700

    add to Main() print console options

--- FlashcardApp/Program.cs
+++ FlashcardApp/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class FlashcardApp
+{
+    public static void Main()
+    {
+        var dbService = new MSSQLDatabase();
+        dbService.ConnectToDB();
+         
+        bool closeApp = false;
+        while(!closeApp)
+        {
+            Console.WriteLine("\n\n\t\t\tYOUR FLASHCARDS STACKS");
+            //GetAllStacks();
+            Console.WriteLine("\n\t\t\tWhat would you like to do?");
+            Console.WriteLine("\nType 0 to close app.");
+            Console.WriteLine("\nType stacks ID to access to stack.");
+            Int32.TryParse(Console.ReadLine(), out int options); 
+            
+            switch(options)
+            {
+                case 0:
+                    closeApp = true;
+                    break;
+                default:
+                    int stackId = options;
+                    // GetAllFLashCardr(int stackId);
+                    break;
+            }
+                
+        }
+    }
+}
