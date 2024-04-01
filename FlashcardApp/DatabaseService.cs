6a2126b99ff09d81d8c688f27b2400dc5172ee23
tien <nq2017.nguyentien081202@gmail.com>
2024-04-01 19:27:43 +0700

    up to date with developing local

--- FlashcardApp/DatabaseService.cs
+++ FlashcardApp/DatabaseService.cs
@@ -13,7 +13,8 @@ internal class MSSQLDatabase
         using(var connection = new SqlConnection(connectionString)){
             connection.Open();
             var tableCmd = connection.CreateCommand();
-            //need to be worked on
+            //The command to create 'stacks' table if
+            //not exists
             string command = @"IF OBJECT_ID('STACKS') IS NULL
                             BEGIN
 	                        CREATE TABLE STACKS
@@ -31,7 +32,10 @@ internal class MSSQLDatabase
             {
                 Console.WriteLine("Database error. Please try again");
             }
-            command = @"IF (OBJECT_ID('CARD') IS NULL AND OBJECT_ID('STACKS') IS NOT NULL)	
+            //The command to create 'cards' table if
+            //not exists and if the 'stacks' table is exists.
+
+            command = @"IF (OBJECT_ID('CARDS') IS NULL AND OBJECT_ID('STACKS') IS NOT NULL)	
                         BEGIN
 	                    CREATE TABLE CARDS
 	                    (
--- FlashcardApp/obj/Debug/net8.0/FlashcardApp.AssemblyInfo.cs
+++ FlashcardApp/obj/Debug/net8.0/FlashcardApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlashcardApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f660e12c026b54c48e77f8c7bf4e1e68d9be8079")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlashcardApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlashcardApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
