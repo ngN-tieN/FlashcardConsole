e32eb59e5c747497eb6a2ffe16e2f77d9be79c4e
tien <nq2017.nguyentien081202@gmail.com>
2024-04-01 21:51:54 +0700

    Format ConnectToDB; add new helper TExecuteNonQuerySQL

--- FlashcardApp/DatabaseService.cs
+++ FlashcardApp/DatabaseService.cs
@@ -10,11 +10,7 @@ internal class MSSQLDatabase
     }
     public void ConnectToDB()
     {
-        using(var connection = new SqlConnection(connectionString)){
-            connection.Open();
-            var tableCmd = connection.CreateCommand();
-            //need to be worked on
-            string command = @"IF OBJECT_ID('STACKS') IS NULL
+        string command = @"IF OBJECT_ID('STACKS') IS NULL
                             BEGIN
 	                        CREATE TABLE STACKS
 	                        (
@@ -22,35 +18,26 @@ internal class MSSQLDatabase
 		                        name varchar(255) unique
 	                        )
                             END";
-            tableCmd.CommandText = command;
-            try
-            {
-                tableCmd.ExecuteNonQuery();
-            }
-            catch (System.Exception)
-            {
-                Console.WriteLine("Database error. Please try again");
-            }
-            command = @"IF (OBJECT_ID('CARD') IS NULL AND OBJECT_ID('STACKS') IS NOT NULL)	
-                        BEGIN
-	                    CREATE TABLE CARDS
-	                    (
-		                    id INT PRIMARY KEY NOT NULL IDENTITY(1,1),
-		                    front TEXT,
-		                    back TEXT, 
-		                    stack_id INT FOREIGN KEY REFERENCES STACKS(id)
-	                    )
-                        END";
-            tableCmd.CommandText = command;
-             try
-            {
-                tableCmd.ExecuteNonQuery();
-            }
-            catch (System.Exception)
-            {
-                Console.WriteLine("Database error. Please try again");
-            }
-            connection.Close();
-        }
+        TExecuteNonQuerySQL(command);
+        command = @"IF (OBJECT_ID('CARD') IS NULL AND OBJECT_ID('STACKS') IS NOT NULL)	
+                    BEGIN
+                    CREATE TABLE CARDS
+                    (
+                        id INT PRIMARY KEY NOT NULL IDENTITY(1,1),
+                        front TEXT,
+                        back TEXT, 
+                        stack_id INT FOREIGN KEY REFERENCES STACKS(id)
+                    )
+                    END";
+        TExecuteNonQuerySQL(command);
+        
+    }
+    public void TExecuteNonQuerySQL(string commandInput)
+    {
+        using var connection = new SqlConnection(connectionString);
+        connection.Open();
+        var tableCmd = connection.CreateCommand();
+        tableCmd.CommandText = commandInput;
+        tableCmd.ExecuteNonQuery();
     }
 }
