c2ed2099843002fab28c008a5277498b2e7eb266
tien <nq2017.nguyentien081202@gmail.com>
2024-04-06 18:01:13 +0700

    add GetALlStacks() methods

--- FlashcardApp/StackController.cs
+++ FlashcardApp/StackController.cs
@@ -2,9 +2,12 @@ using Microsoft.VisualBasic;
 
 internal class StackController
 {
-    static MSSQLDatabase dbService = new();
-    public static void GetAllStacks()
+    private MSSQLDatabase dbService = new();
+    private TableExt tableExtService = new();
+    public void GetAllStacks()
     {
-        
+        string commandInput = "SELECT * FROM STACKS";
+        var stacks = dbService.TDapperGetAllStack(commandInput);
+        tableExtService.printStackTable(stacks);
     }
 }
