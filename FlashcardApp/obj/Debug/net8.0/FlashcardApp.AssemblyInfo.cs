06cb2951552af710349771a4e0cd4f5745916bda
tien <nq2017.nguyentien081202@gmail.com>
2024-04-10 23:17:02 +0700

    add miscellaneous

--- FlashcardApp/obj/Debug/net8.0/FlashcardApp.AssemblyInfo.cs
+++ FlashcardApp/obj/Debug/net8.0/FlashcardApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlashcardApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65ba9dde9397e9da0268a450ac584f5f50e06fb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e30f40a30e0e447e75f8bc831bfe7b0a44bd8f75")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlashcardApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlashcardApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
